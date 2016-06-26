6101a2f86e243c6c58fc535d8d02d7c0c45240f6
Mike Sabens <michael.sabens@me.com>
2016-06-26 15:36:57 -0400

    First Commit

--- Multiples/ViewController.swift
+++ Multiples/ViewController.swift
@@ -9,17 +9,71 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+let maxNumber = 100
+var startingValue: Int = 0
+var userEntry: Int = 0
+var result: Int = 0
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//Outlets
+@IBOutlet weak var logo: UIImageView!
+@IBOutlet weak var userInput: UITextField!
+@IBOutlet weak var playButton: UIButton!
+@IBOutlet weak var resultsLabel: UILabel!
+@IBOutlet weak var addButton: UIButton!
+    
+//Actions
+
+@IBAction func playButtonPressed(sender: UIButton) {
+    
+    if userInput.text != nil && userInput.text != "" {
+        
+        logo.hidden = true
+        userInput.hidden = true
+        playButton.hidden = true
+        
+        resultsLabel.hidden = false
+        addButton.hidden = false
+        
+        userEntry = Int(userInput.text!)!
+        
+        
+    }
+}
+
+@IBAction func addButtonPressed(sender: UIButton) {
+        result = startingValue + userEntry
+        updateResultsLabel()
+    if isGameover() {
+        restartGame()
+    }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+func updateResultsLabel() {
+        resultsLabel.text = "\(startingValue) + \(userEntry) = \(result)"
+        startingValue = startingValue + userEntry
     }
-
+    
+func isGameover() -> Bool {
+        if result >= maxNumber {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func restartGame() {
+        logo.hidden = false
+        userInput.hidden = false
+        playButton.hidden = false
+        
+        resultsLabel.hidden = true
+        addButton.hidden = true
+        
+        startingValue = 0
+        result = 0
+        resultsLabel.text = "Press Add to Add"
+    }
 
 }
 
